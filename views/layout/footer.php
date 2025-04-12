f8dcf16c0e7442c8ec08b65b6819cb8b990a47db
Oswaldo Rivera <oswaldorivera@gmail.com>
2025-04-12 18:24:10 -0300

    Add files via upload

---
+++ views/layout/footer.php
@@ -0,0 +1,6 @@
+    <!-- Scripts JS -->
+    <script src="js/jquery.min.js" type="text/javascript"></script>
+    <script src='<?php echo $_layoutParams['ruta_js']; ?>bootstrap.js'  type="text/javascript"></script>
+  </body>
+  
+</html>
---
+++ views/layout/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Expires" content="0">
+    <meta http-equiv="Last-Modified" content="0">
+    <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    <link rel="stylesheet" href="<?php echo $_layoutParams['ruta_css']; ?>bootstrap.css">
+    <title>
+      SIGA
+    </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  </head>
+
+  <body>
+    <noscript>
+      <p>Para el correcto funcionamiento del sistema, debe tener habilitado el soporte para javascript.</p>
+    </noscript>
